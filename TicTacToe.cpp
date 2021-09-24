a4cd3feeecaf6d23e0de4951ac48ee636357780b
philipknott-cl <philip@cyberline.com>
2021-09-24 10:35:14 -0600

    DisplayBoard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -15,6 +15,27 @@ std::vector<std::vector<int> > CreateBoard() {
     return outer;
 }
 
+void DisplayBoard(std::vector<std::vector<int> > board) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            char c;
+            switch (board[i][j]) {
+                case -1:
+                    c = '.';
+                    break;
+                case 0:
+                    c = 'X';
+                    break;
+                case 1:
+                    c = 'O';
+            }
+            std::cout << c << ' ';
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main() { 
     std::vector<std::vector<int> > board = CreateBoard(); 
+    DisplayBoard(board);
 }

98419c910f3c2b23c681fe3419d52834b14b131e
Vincent Curran <vincentcurran@Vincents-MacBook-Pro-2.local>
2021-09-24 10:54:13 -0600

    display board

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -15,6 +15,21 @@ std::vector<std::vector<int> > CreateBoard() {
     return outer;
 }
 
+void DisplayBoard(std::vector<std::vector<int>> vec) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; i < 3; i++) {
+            if (vec[i][j] == 0) {
+                std::cout << " ";
+            } else if (vec[i][j] == 1) {
+                std::cout << "X";
+            } else {
+                std::cout << "O";
+            }
+        }
+    }
+}
+
 int main() { 
     std::vector<std::vector<int> > board = CreateBoard(); 
+    DisplayBoard(board);
 }
